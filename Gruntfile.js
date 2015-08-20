0a271c651ee7460ae86d00716f754a91e683d0a6
Mark Lybrand <mlybrand@gmail.com>
2015-08-19 23:56:27 -0700

    installed grunt. added grunt file. configured to use mocha for testing

---
+++ Gruntfile.js
@@ -0,0 +1,14 @@
+module.exports = function(grunt) {
+
+    grunt.initConfig({
+        mochaTest: {
+            test: {
+                src: ['test/**/*.js']
+            }
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-mocha-test');
+
+    grunt.registerTask('test', 'mochaTest');
+};
