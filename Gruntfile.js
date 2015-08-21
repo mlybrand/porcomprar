3e68fcf0cf927b89d1edc2e1980bf1893ebdf2c7
Mark Lybrand <mlybrand@gmail.com>
2015-08-21 11:02:46 -0700

    added support to grunt for targeting separate environments (to run integration tests against local and deployed)

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,6 +1,14 @@
 module.exports = function(grunt) {
 
     grunt.initConfig({
+        env: {
+            dev: {
+                NODE_ENV: 'development'
+            },
+            prod: {
+                NODE_ENV: 'production'
+            }
+        },
         jshint: {
             options: {
                expr: true
@@ -14,8 +22,14 @@ module.exports = function(grunt) {
         }
     });
 
+    grunt.loadNpmTasks('grunt-env');
     grunt.loadNpmTasks('grunt-contrib-jshint');
     grunt.loadNpmTasks('grunt-mocha-test');
 
     grunt.registerTask('test', ['jshint', 'mochaTest']);
+    grunt.registerTask('foo', function() {
+        console.log(process.env.NODE_ENV);
+    });
+    grunt.registerTask('bar', ['env:dev', 'foo']);
+    grunt.registerTask('baz', ['env:prod', 'foo']);
 };
