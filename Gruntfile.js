24c9168ccbf87fff0f9209ff27cbce7a3ca71e6a
Mark Lybrand <mlybrand@gmail.com>
2015-08-20 18:55:05 -0700

    added support for jshint. removed colon to make ci build fail

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,14 +1,18 @@
 module.exports = function(grunt) {
 
     grunt.initConfig({
+        jshint: {
+            all: ['*.js']
+        },
         mochaTest: {
             test: {
                 src: ['test/**/*.js']
             }
         }
-    });
+    })
 
+    grunt.loadNpmTasks('grunt-contrib-jshint');
     grunt.loadNpmTasks('grunt-mocha-test');
 
-    grunt.registerTask('test', 'mochaTest');
+    grunt.registerTask('test', ['jshint', 'mochaTest']);
 };
