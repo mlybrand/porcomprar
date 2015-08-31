2f6c160045f28c93f84024f7d2a69a95685c785d
Mark Lybrand <mlybrand@gmail.com>
2015-08-31 00:52:01 -0700

    still trying to get bower stuff working on heroku

--- Gruntfile.js
+++ Gruntfile.js
@@ -99,5 +99,5 @@ module.exports = function(grunt) {
     grunt.registerTask('smoketest', ['env:smoke', 'mochaTest']);
     grunt.registerTask('start:dev', ['env:dev', 'execute']);
     grunt.registerTask('start:prod', ['env:prod', 'bower:install', 'copy', 'execute' ]);
-    grunt.registerTask('heroku', ['bower:install', 'copy', 'execute' ]);
+    grunt.registerTask('heroku', ['env:prod', 'bower:install', 'copy', 'execute' ]);
 };
